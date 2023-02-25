9dc05f491391fb2ab11f6cf93480da6c8d2ed26e
unknown <p.poodi.amer@gmail.com>
2023-02-25 20:10:33 +0330

    i created the data base and admin panel for it

--- saray_restaurant/saray_menu/admin.py
+++ saray_restaurant/saray_menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Food, Categories, Image
 # Register your models here.
+class Category_Model(admin.ModelAdmin):
+    list_display = ["id", "category_name"]
+# class Food_model(admin.ModelAdmin):
+#     list_display = ["id", "food_name", "price", "image", "category", "description"]
+class Image_model(admin.ModelAdmin):
+    list_display = ["id", "image1"]
+class Food_admin(admin.ModelAdmin):
+    filter_horizontal = ("image",)
+admin.site.register(Food, Food_admin)
+admin.site.register(Categories, Category_Model)
+admin.site.register(Image, Image_model)
--- saray_restaurant/saray_menu/models.py
+++ saray_restaurant/saray_menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    category_name = models.CharField(max_length=50)
+    def __str__(self):
+        return f"category: {self.category_name}"
+class Image(models.Model):
+    image1 = models.ImageField(upload_to = "images", blank = True)
+    def __str__(self):
+        return f"{self.image1.name}"
+class Food(models.Model):
+    food_name = models.CharField(max_length = 40)
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    image = models.ManyToManyField(Image, blank=True, related_name="image_field")
+    category = models.ForeignKey(Categories, on_delete = models.CASCADE, related_name = "category_relate")
+    description = models.TextField(max_length = 200, blank = True)
+
+    def __str__(self):
+        p = ""
+        for i in self.image.all():
+            p += f"{str(i)}, "
+        # for i in range(1, Image.objects.count() + 2):
+        #     if self.image in Image.objects.all():
+        #         p += f"{str(Image.objects.get(pk = i))}, "
+        
+        return f"food_name: {self.food_name},  price: {self.price},  image: {p}, {self.category},  description: {self.description} "
